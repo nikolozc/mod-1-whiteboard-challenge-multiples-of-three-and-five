bfc6cbb803106085768c7a0abda418fb0db101d8
Nikolozc <nikolozc@buffalo.edu>
2020-10-30 16:27:01 -0400

    commit tree 6607047e85d2d53fb0c078f6157d5f77dde39916

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,32 @@
-# Enter your object-oriented solution here!
+class Multiples
+
+    attr_accessor :limit
+
+    def initialize(limit)
+        @limit = limit
+    end
+
+def collect_multiples
+    multiples_of_3 = (1...self.limit).to_a.select do |number|
+        number % 3 == 0
+    end
+    multiples_of_5 = (1...self.limit).to_a.select do |number|
+        number % 5 == 0
+    end
+    retVal = (multiples_of_3 + multiples_of_5).uniq!
+    if retVal == nil
+        return (multiples_of_3 + multiples_of_5).sort!
+    else
+        return retVal.sort!
+    end
+end
+
+def sum_multiples
+    array = collect_multiples
+    retVal = 0
+    array.each do |element|
+        retVal+=element
+    end
+    retVal
+end
+end
